5a58aa05255aeb4b5c22bc1328ba4e56e4dabc3e
Ben Bicknell <benabicknell@gmail.com>
2023-04-20 05:44:03 -0400

    15.3

---
+++ clox/vm.c
@@ -0,0 +1,61 @@
+#include "vm.h"
+#include "common.h"
+#include "debug.h"
+#include <stdio.h>
+
+VM vm;
+static void resetStack() { vm.stackTop = vm.stack; }
+
+void initVM() { resetStack(); }
+
+void freeVM() {}
+
+void push(Value value) {
+  *vm.stackTop = value;
+  vm.stackTop++;
+}
+Value pop() {
+  vm.stackTop--;
+  return *vm.stackTop;
+}
+
+static InterpretResult run() {
+#define READ_BYTE() (*vm.ip++)
+#define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
+  for (;;) {
+#ifdef DEBUG_TRACE_EXECUTION
+    printf(" ");
+    for (Value *slot = vm.stack; slot < vm.stackTop; slot++) {
+      printf("[ ");
+      printValue(*slot);
+      printf(" ]");
+    }
+    printf("\n");
+    dissabssembleInstruction(vm.chunk, (int)(vm.ip - vm.chunk->code));
+#endif
+    uint8_t instruction;
+    switch (instruction = READ_BYTE()) {
+    case OP_CONSTANT: {
+      printValue(pop());
+      printf("\n");
+      Value constant = READ_CONSTANT();
+      push(constant);
+      printValue(constant);
+      printf("\n");
+      break;
+    }
+    case OP_RETURN: {
+      return INTERPRET_OK;
+    }
+    }
+  }
+
+#undef READ_BYTE
+#undef READ_CONSTANT
+}
+
+InterpretResult interpret(Chunk *chunk) {
+  vm.chunk = chunk;
+  vm.ip = vm.chunk->code;
+  return run();
+}
