76f09319836ce9cc39f6402dbf2130022985f585
Ben Bicknell <benabicknell@gmail.com>
2023-05-11 11:40:41 -0400

    30

--- clox/chunk.h
+++ clox/chunk.h
@@ -18,6 +18,7 @@ typedef enum {
   OP_SET_UPVALUE,
   OP_GET_PROPERTY,
   OP_SET_PROPERTY,
+  OP_GET_SUPER,
   OP_EQUAL,
   OP_GREATER,
   OP_LESS,
@@ -32,11 +33,13 @@ typedef enum {
   OP_JUMP_IF_FALSE,
   OP_LOOP,
   OP_CLOSURE,
+  OP_SUPER_INVOKE,
   OP_CALL,
   OP_INVOKE,
   OP_CLOSE_UPVALUE,
   OP_RETURN,
   OP_CLASS,
+  OP_INHERIT,
   OP_METHOD
 } OpCode;
 typedef struct {
