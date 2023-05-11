9e05649b496279fc42eb6422c5fd86dcd31a957c
Ben Bicknell <benabicknell@gmail.com>
2023-05-11 10:52:31 -0400

    28.5

--- clox/compiler.c
+++ clox/compiler.c
@@ -29,7 +29,12 @@ typedef struct {
 
 } Upvalue;
 
-typedef enum { TYPE_FUNCTION, TYPE_SCRIPT } FunctionType;
+typedef enum {
+  TYPE_FUNCTION,
+  TYPE_INITIALIZER,
+  TYPE_METHOD,
+  TYPE_SCRIPT
+} FunctionType;
 
 typedef struct Compiler {
   struct Compiler *enclosing;
@@ -41,6 +46,11 @@ typedef struct Compiler {
   Upvalue upvalues[UINT8_COUNT];
   int scopeDepth;
 } Compiler;
+
+typedef struct ClassCompiler {
+  struct ClassCompiler *enclosing;
+} ClassCompiler;
+
 typedef enum {
   PREC_NONE,
   PREC_ASSIGNMENT, // =
@@ -63,6 +73,7 @@ typedef struct {
 } ParseRule;
 
 Compiler *current = NULL;
+ClassCompiler *currentClass = NULL;
 Parser parser;
 Chunk *compilingChunk;
 static Chunk *currentChunk() { return &current->function->chunk; }
@@ -149,7 +160,11 @@ static int emitJump(uint8_t instruction) {
 }
 
 static void emitReturn() {
-  emitByte(OP_NIL);
+  if (current->type == TYPE_INITIALIZER) {
+    emitBytes(OP_GET_LOCAL, 0);
+  } else {
+    emitByte(OP_NIL);
+  }
   emitByte(OP_RETURN);
 }
 
@@ -194,8 +209,13 @@ static void initCompiler(Compiler *compiler, FunctionType type) {
   Local *local = &current->locals[current->localCount++];
   local->depth = 0;
   local->isCaptured = false;
-  local->name.start = "";
-  local->name.length = 0;
+  if (type != TYPE_FUNCTION) {
+    local->name.start = "this";
+    local->name.length = 4;
+  } else {
+    local->name.start = "";
+    local->name.length = 0;
+  }
 }
 
 static ObjFunction *endCompiler() {
@@ -498,6 +518,14 @@ static void variable(bool canAssign) {
   namedVariable(parser.previous, canAssign);
 }
 
+static void this_(bool canAssign) {
+  if (currentClass == NULL) {
+    error("Can't use 'this' outside of a class");
+    return;
+  }
+  variable(false);
+}
+
 static void unary(bool canAssign) {
   TokenType operatorType = parser.previous.type;
 
@@ -550,7 +578,7 @@ ParseRule rules[] = {
     [TOKEN_PRINT] = {NULL, NULL, PREC_NONE},
     [TOKEN_RETURN] = {NULL, NULL, PREC_NONE},
     [TOKEN_SUPER] = {NULL, NULL, PREC_NONE},
-    [TOKEN_THIS] = {NULL, NULL, PREC_NONE},
+    [TOKEN_THIS] = {this_, NULL, PREC_NONE},
     [TOKEN_TRUE] = {literal, NULL, PREC_NONE},
     [TOKEN_VAR] = {NULL, NULL, PREC_NONE},
     [TOKEN_WHILE] = {NULL, NULL, PREC_NONE},
@@ -620,16 +648,39 @@ static void function(FunctionType type) {
   }
 }
 
+static void method() {
+  consume(TOKEN_IDENTIFIER, "Expect method name");
+  uint8_t constant = identifierConstant(&parser.previous);
+  FunctionType type = TYPE_METHOD;
+  if (parser.previous.length == 4 &&
+      memcmp(parser.previous.start, "init", 4) == 0) {
+    type = TYPE_INITIALIZER;
+  }
+  function(type);
+  emitBytes(OP_METHOD, constant);
+}
+
 static void classDeclaration() {
   consume(TOKEN_IDENTIFIER, "Expect class name");
+  Token className = parser.previous;
   uint8_t nameConstant = identifierConstant(&parser.previous);
   declareVariable();
 
   emitBytes(OP_CLASS, nameConstant);
   defineVariable(nameConstant);
 
+  ClassCompiler classCompiler;
+  classCompiler.enclosing = currentClass;
+  currentClass = &classCompiler;
+
+  namedVariable(className, false);
   consume(TOKEN_LEFT_BRACE, "Expect '{' before class body");
+  while (!check(TOKEN_RIGHT_BRACE) && !check(TOKEN_EOF)) {
+    method();
+  }
   consume(TOKEN_RIGHT_BRACE, "Expect '}' after class body");
+  emitByte(OP_POP);
+  currentClass = currentClass->enclosing;
 }
 
 static void funDeclaration() {
@@ -732,6 +783,10 @@ static void returnStatement() {
   if (match(TOKEN_SEMICOLON)) {
     emitReturn();
   } else {
+    if (current->type == TYPE_INITIALIZER) {
+      error("Can't return a value from an initializer");
+    }
+
     expression();
     consume(TOKEN_SEMICOLON, "Expect ';' after return value");
     emitByte(OP_RETURN);
--- clox/memory.c
+++ clox/memory.c
@@ -70,9 +70,16 @@ static void blackenObject(Obj *object) {
   printf("\n");
 #endif
   switch (object->type) {
+  case OBJ_BOUND_METHOD: {
+    ObjBoundMethod *bound = (ObjBoundMethod *)object;
+    markValue(bound->receiver);
+    markObject((Obj *)bound->method);
+    break;
+  }
   case OBJ_CLASS: {
     ObjClass *klass = (ObjClass *)object;
     markObject((Obj *)klass->name);
+    markTable(&klass->methods);
     break;
   }
   case OBJ_CLOSURE: {
@@ -125,7 +132,12 @@ static void freeObject(Obj *object) {
   case OBJ_NATIVE:
     FREE(ObjNative, object);
     break;
+  case OBJ_BOUND_METHOD:
+    FREE(ObjBoundMethod, object);
+    break;
   case OBJ_CLASS: {
+    ObjClass *klass = (ObjClass *)object;
+    freeTable(&klass->methods);
     FREE(ObjClass, object);
     break;
   }
@@ -160,6 +172,7 @@ static void markRoots() {
   }
   markTable(&vm.globals);
   markCompilerRoots();
+  markObject((Obj *)vm.initString);
 }
 
 static void traceReferences() {
